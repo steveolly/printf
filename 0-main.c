ca61550c0ba4f8826f5e1982cd598782a17ea46b
steveolly <ajibolastephen1@gmail.com>
2023-09-19 12:50:09 -0700

    print function

---
+++ 0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - main function.
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+        _printf("Hello, %s!\n", "Have a nice day");
+        _printf("%c is awesome.\n", 'C');
+        _printf("%%%s to you all\n", "Have a nice day");
+
+        return (0);
+}
--- _printf.c
+++ _printf.c
@@ -15,69 +15,61 @@ int _printf(const char *format, ...)
 {
 	va_list args;
 	int count = 0;
+	char *str;
 
-	if (format == NULL)
+	if (!format)
 		return (-1);
 
 	va_start(args, format);
 
+
 	while (*format)
 	{
-	if (*format != '%')
-	{
-		putchar(*format);
-	count++;
-	}
-	else
-	{
-		format++;
-		switch (*format)
-	{
-	case 'c':
-	putchar(va_arg(args, int));
-		count++;
-		break;
-	case 's':
-	{
-		char *str = va_arg(args, char*);
-		int j = 0;
-
-		for (j = 0; str[j]; j++)
+		if (*format != '%')
 		{
-			putchar(str[j]);
+			putchar(*format);
 			count++;
 		}
-		break;
-	}
-	case '%':
-		putchar('%');
-		count++;
-		break;
-	default:
-		putchar('%');
-		putchar(*format);
-		count += 2;
-		break;
+		else
+		{
+			format++;
+			if (!*format)
+				break;
+
+			switch (*format)
+			{
+			case 'c':
+				putchar(va_arg(args, int));
+				count++;
+				break;
+			case 's':
+				str = va_arg(args, char*);
+
+				if (!str)
+					str = "(null)";
+
+				while (*str)
+				{
+					putchar(*str++);
+					count++;
+				}
+				break;
+
+			case '%':
+				putchar('%');
+				count++;
+				break;
+
+			default:
+				putchar('%');
+				putchar(*format);
+				count += 2;
+				break;
+			}
 		}
-	}
-	format++;
+		format++;
 	}
 
 	va_end(args);
 	return (count);
-}
-
-/**
- * main - main function.
- *
- * Return: 0
- */
-
-int main(void)
-{
-	_printf("Hello, %s!\n", "Have a nice day");
-	_printf("%c is awesome.\n", 'C');
-	_printf("%%%s\n", "Have a nice day");
-
-	return (0);
 }
