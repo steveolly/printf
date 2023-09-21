c0f0b415b1b31364ebc54c3f95fcf714a2203240
nolwazi02 <lwazi02mda@gmail.com>
2023-09-20 20:52:00 -0700

    function fix

--- _printf.c
+++ _printf.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include "main.h"
+#include <unistd.h>
 
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 /**
  * printf_char - print a character.
  * @args: the va_list containing the argument.
